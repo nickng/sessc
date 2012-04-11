4f70b3c2e0144d1314e81b642057a63ae1e880d9
Nicholas Ng <nickng@doc.ic.ac.uk>
2012-04-11 18:54:47 +0100

    First commit of communication primitives

--- include/sc/primitives.h
+++ include/sc/primitives.h
@@ -1,4 +1,52 @@
 #ifndef PRIMITIVES_H__
 #define PRIMITIVES_H__
+/**
+ * \file
+ * This file is the communication primitives module header file
+ * of the Session C runtime library (libsc)
+ *
+ */
+
+#include <stdarg.h>
+
+#include "sc/session.h"
+
+/**
+ * \brief Send an integer.
+ *
+ * @param[in] val Value to send
+ * @param[in] r   Role to send to
+ *
+ * \returns 0 if successful, -1 otherwise and set errno
+ *          (See man page of zmq_send)
+ */
+int send_int(int val, role *r);
+
+
+/**
+ * \brief Send an integer to multiple roles.
+ *
+ * @param[in] val         Value to send
+ * @param[in] nr_of_roles Number of roles to send to 
+ * @param[in] ...         Variable number (subject to nr_of_roles)
+ *                        of role variables
+ *
+ * \returns 0 if successful, -1 otherwise and set errno
+ *          (See man page of zmq_recv)
+ */
+int vsend_int(int val, int nr_of_roles, ...);
+
+
+/**
+ * \brief Receive an integer (pre-allocated).
+ *
+ * @param[out] dst Pointer to variable storing recevied value
+ * @param[in]  r   Role to receive from
+ *
+ * \returns 0 if successful, -1 otherwise and set errno
+ *          (See man page of zmq_recv)
+ */
+int recv_int(int *dst, role *r);
+
 
 #endif // PRIMITIVES_H__
--- src/runtime/primitives.c
+++ src/runtime/primitives.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,3 +7,94 @@
 #include <zmq.h>
 
 #include "sc/primitives.h"
+
+
+/**
+ * \brief Helper function to deallocate send queue.
+ *
+ */
+void _dealloc(void *data, void *hint)
+{
+  free(data);
+}
+
+
+int send_int(int val, role *r)
+{
+  int rc = 0;
+  zmq_msg_t msg;
+
+#ifdef __DEBUG__
+  fprintf(stderr, " --> %s(%d) ", __FUNCTION__, val);
+#endif
+
+  int *buf = malloc(sizeof(int));
+  memcpy(buf, &val, sizeof(int));
+
+  zmq_msg_init_data(&msg, buf, sizeof(int), _dealloc, NULL);
+  rc = zmq_send(r, &msg, 0);
+  zmq_msg_close(&msg);
+ 
+#ifdef __DEBUG__
+  fprintf(stderr, ".\n");
+#endif
+
+  return rc;
+}
+
+
+int vsend_int(int val, int nr_of_roles, ...)
+{
+  int rc = 0;
+  int i;
+  role *r;
+  va_list roles;
+  session *s;
+
+#ifdef __DEBUG__
+  fprintf(stderr, " --> %s(%d)@%d ", __FUNCTION__, val, nr_of_roles);
+#endif
+  va_start(roles, nr_of_roles);
+  for (i=0; i<nr_of_roles; i++) {
+    r = va_arg(roles, role *);
+
+    // Are all roles in the same session?
+    if (i==0) s = r->s;
+    assert(s == r->s);
+
+#ifdef __DEBUG__
+    fprintf(stderr, "   +");
+#endif
+    rc |= send_int(val, r);
+  }
+  va_end(roles);
+
+#ifdef __DEBUG__
+  fprintf(stderr, ".\n");
+#endif
+
+  return rc;
+}
+
+
+int recv_int(int *dst, role *r)
+{
+  int rc = 0;
+  zmq_msg_t msg;
+
+#ifdef __DEBUG__
+  fprintf(stderr, " <-- %s() ", __FUNCTION__);
+#endif
+
+  zmq_msg_init(&msg);
+  rc = zmq_recv(r, &msg, 0);
+  assert(zmq_msg_size(&msg) == sizeof(int));
+  memcpy(dst, (int *)zmq_msg_data(&msg), zmq_msg_size(&msg));
+  zmq_msg_close(&msg);
+
+#ifdef __DEBUG__
+  fprintf(stderr, "[%d] .\n", *dst);
+#endif
+
+  return rc;
+}
